9c6e649bbc11717b50148f12dd8dad9603a63e10
Alejandra V <eraseunavezpasteleria@gmail.com>
2020-09-16 17:45:51 -0500

    Taller 11

---
+++ 20200916_Taller_11_Theme/assets/js/script.js
@@ -0,0 +1,62 @@
+
+function getThemeStorage() {
+let btnDark = document.getElementById('dark')
+btnDark.addEventListener('click', () => {
+    //let container = document.getElementById('container').classList.add('dark')
+    // Si selecciona se carga el color de tema en el localstorage
+    localStorage.setItem("SelectedTheme", "dark")
+    getThemeStorage()
+})
+
+let btnlight = document.getElementById('light')
+btnlight.addEventListener('click', () => {
+    //    let container = document.getElementById('container').classList.add('white')
+    // Si selecciona se carga el color de tema en el localstorage
+    localStorage.setItem("SelectedTheme", "light")
+    getThemeStorage()
+})
+
+let btnPink = document.getElementById('pink')
+btnPink.addEventListener('click', () => {
+    //let container = document.getElementById('container').classList.add('pink')
+    // Si selecciona se carga el color de tema en el localstorage
+    localStorage.setItem("SelectedTheme", "pink")
+    getThemeStorage()
+})
+
+
+    let themeOnStorage = localStorage.getItem('SelectedTheme')
+    document.getElementById('container').classList.remove('dark')
+    document.getElementById('container').classList.remove('pink')
+
+    if (themeOnStorage == "dark") {
+        console.log("rule Dark")
+        
+        let container = document.getElementById('container').classList.add('dark')
+    
+    }else if(themeOnStorage == "pink"){
+        console.log("rule pink")
+        let container = document.getElementById('container').classList.add('pink')
+        
+    }else{
+        console.log("rule white")
+        let container = document.getElementById('container').classList.add('white')
+    }
+
+
+}
+
+
+
+//Ejemplos 
+
+const usuario = document.getElementById('usuario')
+usuario.addEventListener('click', () => {
+    const nombreUsuario = prompt('Escribe tu nombre')
+    localStorage.setItem("usuarioStorage", nombreUsuario)
+})
+const traerUsuarioStorage = () => {
+    const usuarioGuardadoEnStorage = localStorage.getItem('usuarioStorage')
+    document.getElementById('mostrarUsuario').innerText = usuarioGuardadoEnStorage
+    console.log(usuarioGuardadoEnStorage, "Este es el usuario")
+}
